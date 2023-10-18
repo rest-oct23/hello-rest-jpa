7c7fe4f8c94608f243063e81c8ce9667cbc21e1c
Javier Arturo Rodriguez <javier@rodriguez.org.mx>
2023-10-18 12:33:35 +0200

    Initial import

---
+++ src/main/java/com/example/hellorestjpa/HelloRestJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.hellorestjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloRestJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloRestJpaApplication.class, args);
+	}
+
+}

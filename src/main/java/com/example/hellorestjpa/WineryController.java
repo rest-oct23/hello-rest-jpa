81cad7c50ab612065335cd7b71eb317957488f5e
Javier Arturo Rodriguez <javier@rodriguez.org.mx>
2023-10-19 10:25:13 +0200

    Added /winery/{id} endpoint

--- src/main/java/com/example/hellorestjpa/WineryController.java
+++ src/main/java/com/example/hellorestjpa/WineryController.java
@@ -1,9 +1,12 @@
 package com.example.hellorestjpa;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -16,6 +19,12 @@ public class WineryController {
     @GetMapping("/")
     List<Winery> getAll() {
         return wineryRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    Winery retrieve(@PathVariable Integer id) throws Exception {
+        return wineryRepository.findById(id)
+                .orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND, "not found"));
     }
 
 }

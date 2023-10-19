7c12d3fd1b2ec1d6469d29fda7ee2c73a5e9e416
Javier Arturo Rodriguez <javier@rodriguez.org.mx>
2023-10-19 10:47:43 +0200

    Added POST /winery endpoint

--- src/main/java/com/example/hellorestjpa/WineryController.java
+++ src/main/java/com/example/hellorestjpa/WineryController.java
@@ -2,11 +2,11 @@ package com.example.hellorestjpa;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+import org.springframework.web.util.UriComponents;
 
 import java.util.List;
 
@@ -16,7 +16,7 @@ public class WineryController {
     @Autowired
     WineryRepository wineryRepository;
 
-    @GetMapping("/")
+    @GetMapping("")
     List<Winery> getAll() {
         return wineryRepository.findAll();
     }
@@ -25,6 +25,17 @@ public class WineryController {
     Winery retrieve(@PathVariable Integer id) throws Exception {
         return wineryRepository.findById(id)
                 .orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND, "not found"));
+    }
+
+    @PostMapping("")
+    ResponseEntity<Winery> create(@RequestBody Winery winery) {
+        winery.setId(null);
+        Winery saved = wineryRepository.save(winery);
+        UriComponents uriComponents = MvcUriComponentsBuilder.fromMethodName(WineryController.class, "retrieve", saved.getId())
+                .buildAndExpand();
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .header("Location", uriComponents.encode().toUriString())
+                .body(saved);
     }
 
 }
